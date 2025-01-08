46d345e12cddb3c1a10ca968069144ae9d371e8a
Akash Srivastava <akashniki@gmail.com>
2025-01-09 00:57:34 +0530

    sm6150-common: Build some missing LDAC libs
    * Bluetooth libs look for them
    
    Signed-off-by: afterallafk <shivamatiet2001@gmail.com>
    Signed-off-by: donjohanliebert <donjohanliebert@gmail.com>

--- sm6150.mk
+++ sm6150.mk
@@ -96,6 +96,10 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml \
     frameworks/native/data/etc/android.hardware.bluetooth.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth.xml
 
+PRODUCT_PACKAGES += \
+    libldacBT_abr \
+    libldacBT_enc
+
 # Camera
 $(call soong_config_set,camera,override_format_from_reserved,true)
 $(call soong_config_set,libcameraservice,ext_lib,//$(LOCAL_PATH):libcameraservice_extension.sm6150)
